b054c748cd9948223eedbb07ccdcb7937e968157
Yazan Al Kassir <yazan.kassir1@gmail.com>
2018-01-23 15:00:16 +0100

    Add files via upload

--- Yazan-Courses Page/script.js
+++ Yazan-Courses Page/script.js
@@ -9,7 +9,8 @@ var Coursesnames = [
   					course3: 'September-October Course'
 
   				}
-  		     ];
+
+  		    ];
 
 var Coursesduration = [
 
@@ -20,7 +21,8 @@ var Coursesduration = [
   					duration3: '6 Weeks'
 
   				}
-  	       	 ];
+
+  	       	];
 
 var Coursescost = [
 
@@ -31,60 +33,65 @@ var Coursescost = [
   					cost3: '200$'
 
   				}
+
             ];
 
-var html = "";
+var html = " ";
 
   for (var i = 0; i < Coursesnames.length; i++) {
 
-  var name1 = Coursesnames[i].course1;
-  var name2 = Coursesnames[i].course2;
-  var name3 = Coursesnames[i].course3;
-var	duration1 = Coursesduration[i].duration1;
-var	duration2 = Coursesduration[i].duration2;
-var	duration3 = Coursesduration[i].duration3;
-var	cost1 = Coursescost[i].cost1;
-var	cost2 = Coursescost[i].cost2;
-var	cost3 = Coursescost[i].cost3;
+	var name1 = Coursesnames[i].course1;
+	var name2 = Coursesnames[i].course2;
+	var name3 = Coursesnames[i].course3;
+	var	duration1 = Coursesduration[i].duration1;
+	var	duration2 = Coursesduration[i].duration2;
+	var	duration3 = Coursesduration[i].duration3;
+	var	cost1 = Coursescost[i].cost1;
+	var	cost2 = Coursescost[i].cost2;
+	var	cost3 = Coursescost[i].cost3;
 
  
-html +=     '<div class="row">';
- html +=      '<div class=" course1 col-lg-4">';
+ html +=    '<div class="row">';
+ html +=    	 '<div class=" course1 col-lg-4">';
 	    html +=	    '<h2>' + name1 + '</h2>';
   		html +=     '<p id="p1">'+'</p>';
-	 html +=      '</div>';
+	 html +=  	 '</div>';
 
-		 html +=  '<div class="course2 col-lg-4">';
-	   html +=		'<h2>' + name2 + '</h2>'
-	   html +=     '<p id="p2">'+'</p>';
+	   html +=  '<div class="course2 col-lg-4">';
+	   html +=	    '<h2>' + name2 + '</h2>'
+	   html +=      '<p id="p2">'+'</p>';
 	 html +=      '</div>';
 	 	html +=   '<div class="course3 col-lg-4">';
 	    html +=		'<h2>' + name3 + '</h2>';
 	    html +=     '<p id="p3">'+'</p>';
-	 html +=      '</div>';
- 			
-	 html +=  '</div>'; 
+	 html +=      '</div>'; 			
+	 html +=    '</div>'; 
 
   	$('#coursesnames_result').html(html);
 
-  }
+ }
+
 
  $('.course1').click(function(){
 
- 	$('#p1').html("This course duration is " + duration1 + " and it costs: " + cost1)
+ 	$('#p1').html("This course duration is " + duration1 + " and it costs: " + cost1);
+ 	$('#p2').html(" ");
+ 	$('#p3').html(" ");
  })
 
   $('.course2').click(function(){
 
- 	$('#p2').html("This course duration is " + duration2 + " and it costs: " + cost2)
+  	$('#p1').html(" ");
+ 	$('#p2').html("This course duration is " + duration2 + " and it costs: " + cost2);
+ 	$('#p3').html(" ");
  })
 
    $('.course3').click(function(){
 
- 	$('#p3').html("This course duration is " + duration3 + " and it costs: " + cost3)
+ 	
+ 	 $('#p1').html(" ");
+ 	 $('#p2').html(" ");
  })
-
-
 
 })
 
