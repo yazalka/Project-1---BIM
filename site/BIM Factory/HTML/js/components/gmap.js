e390a079eb942da2f0053a94a3673265abfb07b3
Kosic Tanja <kosic.tanja@hotmail.com>
2018-01-23 23:20:51 +0100

    missing javascript, a little bit of style and german pages

---
+++ site/BIM Factory/HTML/js/components/gmap.js
@@ -0,0 +1,73 @@
+"use strict";
+
+window.initMap=function() {
+  var customMapType = new google.maps.StyledMapType([
+    {
+      stylers: [
+        {'saturation': -100},
+        {'lightness': 50},
+        {'visibility': 'simplified'}
+      ]
+    },
+    {
+      elementType: 'labels',
+      stylers: [{visibility: 'on'}]
+    },
+    {
+      featureType: 'road',
+      stylers: [{color: '#bbb'}]
+    }
+  ], {
+    name: 
+  });
+
+  var image = new google.maps.MarkerImage(
+  	'img/widgets/gmap-pin.png',
+  	new google.maps.Size(48,54),
+  	new google.maps.Point(0,0),
+  	new google.maps.Point(24,54)
+	);
+
+  var customMapTypeId = 'custom_style';
+
+  var vienna = {lat: 48.196515, lng: 16.359405};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    scrollwheel: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    center: vienna, // Brooklyn.
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+    }
+  });
+
+  var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h1 id="firstHeading" class="firstHeading">Vienna</h1>'+
+    '<div id="bodyContent">'+
+    '<p>277 Bedford Avenue, <br> Vienna, NY 11211, <br> New York, USA</p>'+
+    '</div>'+
+    '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 300
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    clickable: true,
+    icon: image,
+    title: 'Vienna',
+    position: vienna
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
+}
